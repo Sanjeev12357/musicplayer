214699def2cc9f2b67edfe8dd24792fd7730d468
Sanjeev Singh <sanjeevsinghsaini48@gmail.com>
2024-04-10 17:43:31 +0530

    music player

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import TrackPlayer from 'react-native-track-player';
+import { playbackService } from './musicPlayerServices';
+import App from './src/App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+TrackPlayer.registerPlaybackService(() => playbackService);
